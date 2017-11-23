eaaa2fd7f0c39e4604534d1d8088f433a41712e7
Ke Daniel Wu <danielwuke@gmail.com>
2017-11-23 07:00:25 -0500

    add negative example

--- CropCells.m
+++ CropCells.m
@@ -5,9 +5,9 @@ for i=1:100
     img = imread(sprintf('../data/Detection/img%i/img%i.bmp', i, i));
     detection = load(sprintf('../data/Detection/img%i/img%i_detection.mat', i, i));
     detection = detection.detection;
+    %random center for negative example, 10
+    ngCp = randi([14, 487], 10, 2);
     for j = 1:size(detection, 1)
-        %random center for negative example
-        ngCp = [randi([14, 487]) randi([14, 487])];
         c = detection(j, :);
         x = round(c(1));
         y = round(c(2));
@@ -29,24 +29,32 @@ for i=1:100
         end
         %distance from detection point, must > 22 for not covering
         %detection
-        dis = pdist([c; ngCp], 'euclidean');
-        if dis > 22
-            ngPatch = img(ngCp(1)-13:ngCp(1)+13, ngCp(2)-13:ngCp(2)+13);
-            imwrite(ngPatch, sprintf('../negative/%i.bmp', nonCounter));
-            nonCounter = nonCounter + 1;
+        for l = 1:size(ngCp,1)
+            d = pdist([ngCp(l, :);c], 'euclidean');
+            if d < 22
+                ngCp(l, :) = [0 0];
+            end
         end
         patch = img(y1:y2, x1:x2,:);
         imwrite(patch, sprintf('../cell/%i.bmp', counter));
         counter = counter + 1;
     end
     
+    for n = 1:size(ngCp, 1)
+        cp = ngCp(n, :);
+        if ngCp(n, 1) ~= 0
+            nPatch = img(cp(1)-13:cp(1)+13, cp(2)-13:cp(2)+13, :);
+            imwrite(nPatch, sprintf('../negative/%i.bmp', nonCounter));
+            nonCounter = nonCounter + 1;
+        end
+    end
+    
     for k = 1:5
         img = imread(sprintf('../data/Detection/img%i/img%i_%i.bmp', i, i, k));
         detection = load(sprintf('../data/Detection/img%i/img%i_%i_detection.mat', i, i, k));
         detection = detection.detection;
+        ngCp = randi([14, 487], 10, 2);
         for j = 1:size(detection, 1)
-            %random center for negative example
-            ngCp = [randi([14, 487]) randi([14, 487])];
             c = detection(j, :);
             x = round(c(1));
             y = round(c(2));
@@ -66,17 +74,25 @@ for i=1:100
             if y2 == 500
                 y1 = 474;
             end
-            %distance from detection point, must > 22 for not covering
-            %detection
-            dis = pdist([c; ngCp], 'euclidean');
-            if dis > 22
-                ngPatch = img(ngCp(1)-13:ngCp(1)+13, ngCp(2)-13:ngCp(2)+13);
-                imwrite(ngPatch, sprintf('../negative/%i.bmp', nonCounter));
-                nonCounter = nonCounter + 1;
+             %distance from detection point, must > 22 for not covering
+             %detection
+            for l = 1:size(ngCp,1)
+                d = pdist([ngCp(l, :);c], 'euclidean');
+                if d < 22
+                    ngCp(l, :) = [0 0];
+                end
             end
             patch = img(y1:y2, x1:x2,:);
             imwrite(patch, sprintf('../cell/%i.bmp', counter));
             counter = counter + 1;
+        end
+        for n = 1:size(ngCp, 1)
+            cp = ngCp(n, :);
+            if ngCp(n, 1) ~= 0
+                nPatch = img(cp(1)-13:cp(1)+13, cp(2)-13:cp(2)+13, :);
+                imwrite(nPatch, sprintf('../negative/%i.bmp', nonCounter));
+                nonCounter = nonCounter + 1;
+            end
         end
     end
     
